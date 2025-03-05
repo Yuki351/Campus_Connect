9964f57ebc9ec60fadc40dc552507e00500e4438
Yuki351 <yukikazukabe@gmail.com>
2025-03-05 15:41:51 +0700

    week-03

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.starter');
 });
+
+Route::get('/1', function () {
+    return view('demo.file1');
+});
+
+Route::get('/2', [DemoController::class, 'index']);
+
+Route::get('/3', function () {
+    return view('demo.file3');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
